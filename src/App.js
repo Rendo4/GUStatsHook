dbea3758709d81956718b8a3f2c734bfcb0f3511
Ryan Rendon <Rendon.S.Ryan@gmail.com>
2020-10-11 00:25:04 -0700

    added more styling and buttons to change page of reresults

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import useFetch from "./hooks/useFetch";
 import "./App.css";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(1);
   const { response, error, loading } = useFetch(
     "http://localhost:5000/api/players",
     {
       query: {
-        page: 1,
+        page: currentPage,
         pageSize: 10,
       },
     }
@@ -21,14 +22,35 @@ function App() {
   }
   return (
     <div className="App">
-      {response.map((data) => {
-        return (
-          <div className="datapoint" key={data.summonerID}>
+      <h1>Koshi Midseasonal Invetaional - Gold Under - Page {currentPage}</h1>
+      {loading && <div className="loading">Loading page {currentPage}</div>}
+      {!loading &&
+        response.map((data) => {
+          return (
+            <div className="datapoint" key={data.summonerID}>
             <h3>{data.summoner}</h3>
             {data.team} VS. {data.opponent}
           </div>
         );
       })}
+      <div className="pagination">
+        {currentPage > 1 && (
+          <button
+            onClick={() => {
+              setCurrentPage(currentPage - 1);
+            }}
+          >
+            Go to page {currentPage - 1}
+          </button>
+        )}
+        <button
+          onClick={() => {
+            setCurrentPage(currentPage + 1);
+          }}
+        >
+          Go to page {currentPage + 1}
+        </button>
+      </div>
     </div>
   );
 }
